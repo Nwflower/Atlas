b79cfa4059c1efcb6d93c2982ebfd9a912265a39
西北一枝花 <1679659@qq.com>
2022-11-27 16:44:50 +0800

    新增两种匹配规则

--- atlas.js
+++ atlas.js
@@ -3,12 +3,11 @@ import fs from 'node:fs'
 import { segment } from 'oicq'
 import YAML from 'yaml'
 import gsCfg from '../genshin/model/gsCfg.js'
-
 // 插件制作 西北一枝花(1679659) 首发群240979646，不准搬，一旦在其他群看到本插件立刻停止所有插件制作
 export class atlas extends plugin {
   constructor () {
     let rule = {
-      reg: '#?[\u4e00-\u9fa5]+(图鉴)*',
+      reg: '#?.+(图鉴)*',
       fnc: 'atlas'
     }
     super({
@@ -21,11 +20,11 @@ export class atlas extends plugin {
     this._path = process.cwd().replace(/\\/g, '/')
     this.islog = false
     Object.defineProperty(rule, 'log', { get: () => this.islog })
-    this.version = '3.3'
+    this.version = '3.3.1'
   }
 
   get pluginName () {
-    if (!fs.existsSync('./plugins/Atlas')) { return 'atlas' }
+    if (!fs.existsSync(`${this._path}/plugins/Atlas`)) { return 'atlas' }
     return 'Atlas'
   }
 
@@ -36,34 +35,20 @@ export class atlas extends plugin {
 
   async atlas (e) {
     let msg
-    try { msg = e.msg.replaceAll(/图鉴/g, '').trim() } catch (e) {
+    try { msg = e.msg.trim() } catch (e) {
       logger.debug(e)
       return false
     }
     if (fs.existsSync(`${this._path}/plugins/${this.pluginName}/Genshin-Atlas`)) {
-      const syncFiles = fs.readdirSync(`${this._path}/plugins/${this.pluginName}/Genshin-Atlas`).filter(function (item, index, arr) {
-        return item !== '.git'
-      })
+      const syncFiles = fs.readdirSync(`${this._path}/plugins/${this.pluginName}/Genshin-Atlas`).filter(function (item, index, arr) { return item !== '.git' })
       for (let sync of syncFiles) {
-        let pickrule = await this.getRule(sync)
-        if (Array.isArray(pickrule.pickreg)) {
-          let flag = false
-          pickrule.pickreg.forEach((val) => { if (!val.test(msg)) { flag = true } })
-          if (flag) { continue }
-        } else { if (!pickrule.pickreg.test(msg)) { continue } }
-        let Tmpmsg = msg.replaceAll(pickrule.reg, '').trim()
-        let Dir = fs.statSync(`${this._path}/plugins/${this.pluginName}/Genshin-Atlas/${sync}`)
-        if (Dir.isDirectory()) {
-          // 角色材料特殊处理
-          if (sync === 'material for role') {
-            let rolename = gsCfg.getRole(Tmpmsg, '突破|材料|素材|更新')
-            if (!rolename) continue
-            if (['10000005', '10000007', '20000000'].includes(String(rolename.roleId))) { return await this.e.reply('暂无主角素材') }
-            Tmpmsg = rolename.name
-          }
-          let path = `${this._path}/plugins/${this.pluginName}/Genshin-Atlas/${sync}/${await this.getName(Tmpmsg, sync)}.png`
+        let rule = await this.getRule(sync)
+        let Tmpmsg = await this.PickRule(msg, rule)
+        if (!Tmpmsg) { continue }
+        if (fs.statSync(`${this._path}/plugins/${this.pluginName}/Genshin-Atlas/${sync}`).isDirectory()) {
+          let path = `${this._path}/plugins/${this.pluginName}/Genshin-Atlas/${sync}/${await this.getName(Tmpmsg, sync, rule.mode)}.png`
           if (fs.existsSync(path)) {
-            e.reply(segment.image(path))
+            this.reply(segment.image(path))
             this.islog = true
           }
         }
@@ -72,42 +57,42 @@ export class atlas extends plugin {
     return this.islog
   }
 
-  // 获取匹配规则
   async getRule (sync) {
     let syncPath = `${this._path}/plugins/${this.pluginName}/resource/rule/${sync}.yaml`
     if (!fs.existsSync(syncPath)) { syncPath = `${this._path}/plugins/${this.pluginName}/resource/rule/config.yaml` }
-    let YamlObject = YAML.parse(fs.readFileSync(syncPath, 'utf8'))
-    let pickreg = /#?[\u4e00-\u9fa5]+(图鉴)*/g
-    let reg = /.*/g
-    if (YamlObject.condition) {
-      if (YamlObject.condition === 1) {
-        reg = /#/g
-        pickreg = /^#+/g
-      } else if (YamlObject.condition === 2) {
-        pickreg = new RegExp(`(${YamlObject.pick.join('|')})+`, 'g')
-        YamlObject.pick.push('#')
-        reg = new RegExp(`(${YamlObject.pick.join('|')})+`, 'g')
-      } else if (YamlObject.condition === 3) {
-        YamlObject.pick.push('#')
-        pickreg = new RegExp(`(${YamlObject.pick.join('|')})+`, 'g')
-        reg = pickreg
-      } else {
-        pickreg = [/^#/g, new RegExp(`(${YamlObject.pick.join('|')})+`, 'g')]
-        YamlObject.pick.push('#')
-        reg = new RegExp(`(${YamlObject.pick.join('|')})+`, 'g')
-      }
-    }
-    return {
-      mode: YamlObject.mode,
-      reg,
-      pickreg
-    }
+    return YAML.parse(fs.readFileSync(syncPath, 'utf8'))
   }
 
-  async getName (originName, sync) {
+  async PickRule (msg, Rule) {
+    let pickreg = /图鉴/g
+    if ('pick' in Rule) { pickreg = new RegExp(`(${Rule.pick.join('|')})`, 'g') }
+    switch (Rule.condition) {
+      case 0:// 去除可能的前缀#
+        return msg.replaceAll('#', '').trim()
+      case 1:// 匹配前缀#并去除
+        if (/^#.*$/g.test(msg)) { return msg.replaceAll('#', '').trim() }
+        return false
+      case 2:// 匹配关键字并去除
+        if (pickreg.test(msg)) { return msg.replaceAll('#', '').replaceAll(pickreg, '').trim() }
+        return false
+      case 3:// 匹配关键字或前缀#并去除
+        if (pickreg.test(msg) || /^#.*$/g.test(msg)) { return msg.replaceAll('#', '').replaceAll(pickreg, '').trim() }
+        return false
+      case 4:// 匹配关键字和前缀#并去除
+        if (pickreg.test(msg) && /^#.*$/g.test(msg)) { return msg.replaceAll('#', '').replaceAll(pickreg, '').trim() }
+    }
+    return false
+  }
+
+  async getName (originName, sync, pickmode) {
     if (fs.existsSync(`${this._path}/plugins/${this.pluginName}/resource/othername/${sync}.yaml`)) {
       let YamlObject = YAML.parse(fs.readFileSync(`${this._path}/plugins/${this.pluginName}/resource/othername/${sync}.yaml`, 'utf8'))
-      for (let element in YamlObject) { if (YamlObject[element].includes(originName)) { return element } }
+      for (let element in YamlObject) { if (pickmode) { if (pickmode === 1) { for (let Elementword of YamlObject[element]) { if (Elementword.includes(originName)) { return element } } } else { for (let Elementword of YamlObject[element]) { if (originName.includes(Elementword)) { return element } } } } else { if (YamlObject[element].includes(originName)) { return element } } }
+    }
+    // 角色材料特殊处理
+    if (sync === 'material for role') {
+      let rolename = gsCfg.getRole(originName, '突破|材料|素材|更新')
+      if (rolename) return rolename.name
     }
     return originName
   }
