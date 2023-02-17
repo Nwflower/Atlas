65ba879cece9ca329ac91a903ee15a8e424ca71d
西北一枝花 <1679659@qq.com>
2023-02-17 18:31:03 +0800

    兼容多图鉴

---
+++ model/moreLib.js
@@ -0,0 +1,19 @@
+
+//多图鉴兼容模块
+const list = ["原神","崩铁"]
+
+const library = {
+  "原神":"Genshin-Atlas",
+  "崩铁":"star-rail-atlas"
+}
+
+const link = {
+  "原神":"https://gitee.com/Nwflower/genshin-atlas",
+  "崩铁":"https://gitee.com/Nwflower/star-rail-atlas"
+}
+
+export {
+  list,
+  library,
+  link
+}
