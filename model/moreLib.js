43b6a6a3c94c8c1408c1278c4f06bc731dcbaf2e
Nwflower <1679659@qq.com>
2023-11-25 22:38:24 +0800

    绝区零图鉴支持

--- model/moreLib.js
+++ model/moreLib.js
@@ -1,19 +1,23 @@
 
 // 多图鉴兼容模块
-const list = ['github原神', 'github星铁', '原神', '星铁']
+const list = ['github原神', 'github星铁','github绝区零', '原神', '星铁','绝区零',]
 
 const library = {
   原神: 'Genshin-Atlas',
   github原神: 'Genshin-Atlas',
   星铁: 'star-rail-atlas',
   github星铁: 'star-rail-atlas'
+  绝区零: 'zzz-atlas',
+  github绝区零: 'zzz-atlas'
 }
 
 const link = {
   原神: 'https://gitee.com/Nwflower/genshin-atlas',
   星铁: 'https://gitee.com/Nwflower/star-rail-atlas',
+  绝区零: 'https://gitee.com/Nwflower/zzz-atlas',
   github原神: 'https://github.com/Nwflower/genshin-atlas',
   github星铁: 'https://github.com/Nwflower/star-rail-atlas'
+  github绝区零: 'https://github.com/Nwflower/zzz-atlas'
 }
 
 export {
